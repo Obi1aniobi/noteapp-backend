17aa916d66459d1c6af518fc15d90054cb53eebe
Siddharth Roychoudhury <debu2in@gmail.com>
2023-06-01 20:39:26 +0200

    changed router and added README.MD

--- notes/notes.router.js
+++ notes/notes.router.js
@@ -1,6 +1,7 @@
 const schoolsRouter = require("express").Router();
 const { connect } = require("../app-data-source")
 const notesEntity = require("./notes.entity") 
+const usersEntity = require("./users.entity") 
 
 
 notesRouter.get("/", async(req,res)=>{
@@ -18,12 +19,14 @@ notesRouter.get("/", async(req,res)=>{
     }
 })
 
-notesRouter.post("/", async(req,res)=>{
+notesRouter.post("/user/:id", async(req,res)=>{
     let message = "", status;
+    const id = req.params.id;
     try{
         const connection = await connect()
-        const schoolsData = await connection.getRepository(notesEntity).create(req.body)
-        const results = await connection.getRepository(notesEntity).save(schoolsData)
+        const user = await connection.getRepository(usersEntity).findOne({id}, {relations: ['notes']})
+        user.notes = [...user.notes, {note:req.body.note}]
+        const results = await connection.getRepository(usersEntity).save(user)
         status = 200;
         message = results;
     }catch(e){
